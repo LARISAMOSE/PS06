ce0217107a61d525b00a76b0ca1edfca88096f74
LARISAMOSE <mosevica@gmail.com>
2024-08-27 23:57:29 +0200

    add

--- Part1.py
+++ Part1.py
@@ -1,11 +1,31 @@
-data = [
-    [100, 120, 130],
-    [400, 500, 600],
-    [150, 140, 110]
-    ]
-list =[]
-for row in data:
-    for item in row:
-        if item > 190:
-            list.append(item)
-print(list)
+import time
+import csv
+from selenium import webdriver
+from  selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+url = "https://tomsk.hh.ru/vacancies/programmist"
+driver.get(url)
+time.sleep(30)
+
+vacancies = driver.find_elements(By.CLASS_NAME, "vacancy-card--H8LvOiOGPll0jZvYpxIF")
+parsed_data = []
+
+for vacancy in vacancies:
+    try:
+        title = vacancy.find_element(By.CSS_SELECTOR, "span.vacancy-name--SYbxrgpHgHedVTkgI_cA").text
+        company = vacancy.find_element(By.CSS_SELECTOR, 'span.company-info-text--O32pGCRW0YDmp3BHuNOP').text
+        salary = vacancy.find_element(By.CSS_SELECTOR, 'span.compensation-text--cCPBXayRjn5GuLFWhGTJ').text
+        link = vacancy.find_element(By.CSS_SELECTOR, 'a.bloko-link').get_attribute('href')
+    except:
+        print("Произошла ошибка при парсинге")
+        continue
+    parsed_data.append([title, company, salary, link])
+driver.quit()
+with open("hh.csv", 'w',newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Название вакансии', 'название компании', 'зарплата', 'ссылка на вакансию'])
+    writer.writerows(parsed_data)
+
+
